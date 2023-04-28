e051c6cb7faac827d3b1078160504c703e44b0e1
Nick Rout <nicholasrout@gmail.com>
2023-04-28 11:31:52 +0200

    Prepare library for Maven Central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,17 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.gradle.maven.publish) apply false
 }
 
 subprojects {
-    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
-    extensions.configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    apply<SpotlessPlugin>()
+    extensions.configure<SpotlessExtension> {
         kotlin {
             target("**/*.kt")
             targetExclude("$buildDir/**/*.kt")
--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.gradle.maven.publish)
 }
 
 android {
@@ -22,6 +25,41 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.S01)
+    signAllPublications()
+    coordinates(
+        groupId = "dev.ricknout.composesensors",
+        artifactId = "composesensors",
+        version = "1.0.0-SNAPSHOT"
+    )
+    pom {
+        name.set("Compose Sensors")
+        description.set("A thin wrapper around the Android Sensor APIs, designed to make it easier to work with them in Jetpack Compose.")
+        inceptionYear.set("2023")
+        url.set("https://github.com/ricknout/compose-sensors")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("ricknout")
+                name.set("Nick Rout")
+                url.set("https://github.com/ricknout")
+            }
+        }
+        scm {
+            url.set("https://github.com/ricknout/compose-sensors")
+            connection.set("scm:git:git://github.com/ricknout/compose-sensors.git")
+            developerConnection.set("scm:git:ssh://git@github.com/ricknout/compose-sensors.git")
+        }
     }
 }
 
